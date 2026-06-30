952830987b202dc6b697fef5bcf958e246e7deac
Marco <manuelmarkvince@gmail.com>
2026-06-30 12:51:14 +0800

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-screen flex flex-col">
         <ThemeProvider>
           <ThemeChanger />
-          <div className="mb-20">{children}</div>
+          <main>
+
+          {children}
+          </main>
 
           <NavBar />
         </ThemeProvider>
