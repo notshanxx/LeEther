5ef4920be7ee2f3586fb126b4842fd8b0325b8ae
Marco <manuelmarkvince@gmail.com>
2026-06-30 12:48:55 +0800

    fix dock covering the content

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -6,12 +6,9 @@ export default function DiscoverPage() {
   
 
   return (
-    <div className="flex flex-col items-center justify-center mt-24">
+    <div className="flex flex-col items-center mt-24 pb-24">
       <h1 className="text-4xl font-bold mb-10">Discover Page</h1>
-      {/* <p className="mt-4 text-lg text-gray-600">
-        This is the discover page of the application.
-      </p> */}
-      <div className="flex flex-col sm:flex-row gap-4 mx-5 max-w-5xl">
+      <div className="flex flex-col sm:flex-row gap-4 mx-5 max-w-2xl">
         {/* list here */}
         <FeatureLists />
         <MiniTestList />
