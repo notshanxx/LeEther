af7ca81feedacb470e301f7e0a6d23226458e257
Marco <manuelmarkvince@gmail.com>
2026-06-30 13:55:17 +0800

    remove unused helper

--- lib/western-zodiac.ts
+++
@@ -1,9 +0,0 @@
-import zodiacs from "@/data/western-zodiac.json";
-
-export function getZodiac(id: string){
-  return zodiacs.find((z) => z.id === id);
-}
-
-export function getAllZodiacs(){
-  return zodiacs;
-}
