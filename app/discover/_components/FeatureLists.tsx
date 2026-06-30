8e5add7a2d3c4b111721f8198cd713d82aae96f7
Marco <manuelmarkvince@gmail.com>
2026-06-30 13:55:44 +0800

    remove unused useState

--- app/discover/_components/FeatureLists.tsx
+++ app/discover/_components/FeatureLists.tsx
@@ -1,7 +1,6 @@
 "use client";
 import DynamicImage from "@/app/_components/DynamicImage";
 import Link from "next/link";
-import { useState } from "react";
 export default function FeatureLists() {
   const features = [
     {
